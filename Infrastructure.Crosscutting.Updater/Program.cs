a3af69956758d8601fb5470fa6eea94f13fff337
jiguixin <jiguixin_1008@qq.com>
2012-04-06 16:34:30 +0800

    Signed-off-by: jiguixin <jiguixin_1008@qq.com>

--- Infrastructure.Crosscutting.Updater/Program.cs
+++ Infrastructure.Crosscutting.Updater/Program.cs
@@ -27,13 +27,7 @@ namespace Infrastructure.Crosscutting.Updater
 			{
 				new Dialogs.SelfUpdate().ShowDialog();
 				return;
-			}
-			else
-			{
-			    Updater updater = Updater.Instance;
-                updater.Error += new EventHandler(updater_Error);
-                Updater.CheckUpdateSimple(args[0]); 
-			}
+			} 
 			//升级需要的参数
 			// 主程序版本 主程序目录 升级信息路径 [*主程序ID|主程序进程名]
 			Application.Run(new MainWindow());
